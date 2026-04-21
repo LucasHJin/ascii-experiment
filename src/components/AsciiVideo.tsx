52b7e6711b57775026ebadebdefdafba33cc535a
Lucas Jin <kblazer20@gmail.com>
2026-04-21 11:28:54 -0400

    feat: add color using visible canvas

--- src/components/AsciiVideo.tsx
+++ src/components/AsciiVideo.tsx
@@ -1,32 +1,44 @@
 import { useRef, useEffect } from "react";
 
-const SCALE = 0.15;
+const SCALE = 0.05;
+
+// Overall Notes
+    // Color doesn't show up that well (need darker background)
+    // Individual characters get blurry at smaller font size + very laggy very quickly at higher scales
+
+// IMPROVE SCALING
 
 function AsciiVideo() {
     const hiddenCanvasRef = useRef<HTMLCanvasElement | null>(null);
     const videoRef = useRef<HTMLVideoElement | null>(null);
-    const preRef = useRef<HTMLPreElement | null>(null);
+    //const preRef = useRef<HTMLPreElement | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
     useEffect(() => {
         const video = videoRef.current;
         const hiddenCanvas = hiddenCanvasRef.current;
-        if (!video || !hiddenCanvas) return;
+        const canvas = canvasRef.current;
+        if (!video || !hiddenCanvas || !canvas) return;
 
-        const ctx = hiddenCanvas.getContext("2d");
-        if (!ctx) return;
+        const hiddenCtx = hiddenCanvas.getContext("2d");
+        const visibleCtx = canvas.getContext("2d");
+        if (!hiddenCtx || !visibleCtx) return;
         
         let animFrameId: number;
+        const BLOCK_SIZE = 1;
+        const CHARS = ' .,:;i1tfLCG08@';
+        const FONT_SIZE = 12;
 
         const processFrame = () => {
             const { width, height } = hiddenCanvas;
 
-            ctx.drawImage(video, 0, 0, width, height);
-            const imageData = ctx.getImageData(0, 0, width, height);
+            hiddenCtx.drawImage(video, 0, 0, width, height);
+            const imageData = hiddenCtx.getImageData(0, 0, width, height);
             const pixels = imageData.data;
 
-            const BLOCK_SIZE = 1;
-            const CHARS = ' .,:;i1tfLCG08@';
-            let result = '';
+            visibleCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+            //let result = "";
 
             for (let y = 0; y < height; y += BLOCK_SIZE) {
                 for (let x = 0; x < width; x += BLOCK_SIZE) {
@@ -34,11 +46,16 @@ function AsciiVideo() {
                     const r = pixels[i], g = pixels[i+1], b = pixels[i+2];
                     const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
                     const charIndex = Math.floor((brightness / 255) * (CHARS.length - 1)); // normalize and find corresponding index
-                    result += CHARS[charIndex];
+                    const char = CHARS[charIndex];
+                    
+                    visibleCtx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+                    visibleCtx.fillText(char, x * FONT_SIZE, y * FONT_SIZE);
+                    
+                    //result += CHARS[charIndex];
                 }
-                result += '\n';
+                //result += '\n';
             }
-            if (preRef.current) preRef.current.textContent = result;
+            //if (preRef.current) preRef.current.textContent = result;
         };
 
         const loop = () => {
@@ -49,6 +66,13 @@ function AsciiVideo() {
         const onLoaded = () => {
             hiddenCanvas.width = Math.floor(video.videoWidth * SCALE);
             hiddenCanvas.height = Math.floor(video.videoHeight * SCALE);
+
+            canvas.width = hiddenCanvas.width * FONT_SIZE;
+            canvas.height = hiddenCanvas.height * FONT_SIZE;
+
+            visibleCtx.font = `${FONT_SIZE}px monospace`
+            visibleCtx.textBaseline = "top";
+
             video.play();
             animFrameId = requestAnimationFrame(loop);
         };
@@ -69,11 +93,15 @@ function AsciiVideo() {
                 <source src="/test.mp4" type="video/mp4" />
             </video>
 
+            <canvas ref={canvasRef} style={{ background: 'black' }} />
+
+            {/*
             <pre ref={preRef} style={{ 
                 fontSize: '6px', 
                 lineHeight: '3px',
                 fontFamily: 'monospace'
             }}/>
+            */}
         </div>
     );
 }
