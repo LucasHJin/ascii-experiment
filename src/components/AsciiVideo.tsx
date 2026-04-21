029e07d828f9aa69f3b8c3d0d2a9c121eb24b810
Lucas Jin <kblazer20@gmail.com>
2026-04-21 11:56:01 -0400

    feat: canvas ascii auto scaling by font size

--- src/components/AsciiVideo.tsx
+++ src/components/AsciiVideo.tsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from "react";
 
-const SCALE = 0.05;
+const FONT_SIZE = 10;
 
 // Overall Notes
     // Color doesn't show up that well (need darker background)
@@ -13,21 +13,24 @@ function AsciiVideo() {
     const videoRef = useRef<HTMLVideoElement | null>(null);
     //const preRef = useRef<HTMLPreElement | null>(null);
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
 
     useEffect(() => {
         const video = videoRef.current;
         const hiddenCanvas = hiddenCanvasRef.current;
         const canvas = canvasRef.current;
-        if (!video || !hiddenCanvas || !canvas) return;
+        const container = containerRef.current
+        if (!video || !hiddenCanvas || !canvas || !container) return;
 
         const hiddenCtx = hiddenCanvas.getContext("2d");
         const visibleCtx = canvas.getContext("2d");
         if (!hiddenCtx || !visibleCtx) return;
         
         let animFrameId: number;
-        const BLOCK_SIZE = 1;
         const CHARS = ' .,:;i1tfLCG08@';
-        const FONT_SIZE = 12;
+        let charW: number;
+        let charH: number;
 
         const processFrame = () => {
             const { width, height } = hiddenCanvas;
@@ -40,8 +43,8 @@ function AsciiVideo() {
 
             //let result = "";
 
-            for (let y = 0; y < height; y += BLOCK_SIZE) {
-                for (let x = 0; x < width; x += BLOCK_SIZE) {
+            for (let y = 0; y < height; y++) {
+                for (let x = 0; x < width; x++) {
                     const i = (y * width + x) * 4; // [r, g, b, a] -> need to multiply by 4
                     const r = pixels[i], g = pixels[i+1], b = pixels[i+2];
                     const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
@@ -49,7 +52,7 @@ function AsciiVideo() {
                     const char = CHARS[charIndex];
                     
                     visibleCtx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-                    visibleCtx.fillText(char, x * FONT_SIZE, y * FONT_SIZE);
+                    visibleCtx.fillText(char, x * charW, y * charH);
                     
                     //result += CHARS[charIndex];
                 }
@@ -64,13 +67,26 @@ function AsciiVideo() {
         };
 
         const onLoaded = () => {
-            hiddenCanvas.width = Math.floor(video.videoWidth * SCALE);
-            hiddenCanvas.height = Math.floor(video.videoHeight * SCALE);
-
-            canvas.width = hiddenCanvas.width * FONT_SIZE;
-            canvas.height = hiddenCanvas.height * FONT_SIZE;
-
             visibleCtx.font = `${FONT_SIZE}px monospace`
+            visibleCtx.textBaseline = "top";
+
+            charW = visibleCtx.measureText('A').width;
+            charH = FONT_SIZE;
+            const charAspect = charW / charH;
+
+            const cols = Math.floor(container.clientWidth / charW);
+            const rows = Math.floor(container.clientHeight / charH);
+
+            const scale = Math.min(cols / video.videoWidth, rows / video.videoHeight); // max for cutoff extra
+
+            hiddenCanvas.width = Math.floor(video.videoWidth * scale / charAspect); // compensate for character aspect ratio
+            hiddenCanvas.height = Math.floor(video.videoHeight * scale);
+
+            canvas.width = hiddenCanvas.width * charW;
+            canvas.height = hiddenCanvas.height * charH;
+
+            // re-apply after resize (it gets reset)
+            visibleCtx.font = `${FONT_SIZE}px monospace`;
             visibleCtx.textBaseline = "top";
 
             video.play();
@@ -86,14 +102,17 @@ function AsciiVideo() {
     }, []);
 
     return (
-        <div>
+        <div 
+            ref={containerRef}
+            style={{ width: '100vw', height: '100vh', background: 'black', overflow: 'hidden', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
+        >
             <canvas ref={hiddenCanvasRef} style={{ display: "none" }} />
 
             <video ref={videoRef} muted playsInline autoPlay style={{ display: "none" }}>
                 <source src="/test.mp4" type="video/mp4" />
             </video>
 
-            <canvas ref={canvasRef} style={{ background: 'black' }} />
+            <canvas ref={canvasRef} style={{ display: 'block' }} />
 
             {/*
             <pre ref={preRef} style={{ 
