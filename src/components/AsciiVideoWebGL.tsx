a6a3687c6963b5efcb0ad60c8d39ff5dc3744bf6
Lucas Jin <kblazer20@gmail.com>
2026-04-23 21:14:33 -0400

    feat: draw image with texture

--- src/components/AsciiVideoWebGL.tsx
+++ src/components/AsciiVideoWebGL.tsx
@@ -2,18 +2,21 @@ import { useRef, useEffect } from "react";
 
 const vertSrc = `
 attribute vec2 a_position;
+attribute vec2 a_texCoord;
+varying vec2 v_texCoord;
 
 void main() {
     gl_Position = vec4(a_position, 0.0, 1.0);
+    v_texCoord = a_texCoord;
 }
 `;
 const fragSrc = `
 precision mediump float;
-uniform vec2 u_resolution;
+uniform sampler2D u_texture;
+varying vec2 v_texCoord;
 
 void main() {
-    vec2 uv = gl_FragCoord.xy / u_resolution; // normalized coordinate for 2D space
-    gl_FragColor = vec4(step(0.5, uv.x), step(0.5, uv.y), 0.0, 1.0);
+    gl_FragColor = texture2D(u_texture, v_texCoord);
 }
 `;
 
@@ -54,6 +57,9 @@ function AsciiVideoWebGL() {
         canvas.width = canvas.clientWidth;
         canvas.height = canvas.clientHeight;
 
+        const image = new Image();
+        image.src = "/test.jpg";
+
         const gl = canvas.getContext("webgl");
         if (!gl) return;
         const vertShader = createShader(gl, gl.VERTEX_SHADER, vertSrc);
@@ -63,28 +69,44 @@ function AsciiVideoWebGL() {
         const program = createProgram(gl, vertShader, fragShader);
         if (!program) return;
         
+        // x, y, u, v
         const data = new Float32Array([
-            1.0, 1.0,
-            -1.0, 1.0,
-            1.0, -1.0,
-            -1.0, -1.0,
-            -1.0, 1.0,
-            1.0, -1.0,
+            1.0,  1.0,  1.0, 0.0,
+            -1.0,  1.0,  0.0, 0.0,
+            1.0, -1.0,  1.0, 1.0,
+            -1.0, -1.0,  0.0, 1.0,
+            -1.0,  1.0,  0.0, 0.0,
+            1.0, -1.0,  1.0, 1.0,
         ]);
         const buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
 
         const posLoc = gl.getAttribLocation(program, "a_position");
-        const resLoc = gl.getUniformLocation(program, "u_resolution");
-
-        gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 0, 0); 
+        const uvLoc = gl.getAttribLocation(program, "a_texCoord");
+        gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 16, 0); 
+        gl.vertexAttribPointer(uvLoc, 2, gl.FLOAT, false, 16, 8);
         gl.enableVertexAttribArray(posLoc);
+        gl.enableVertexAttribArray(uvLoc);
 
         gl.useProgram(program);
-        gl.uniform2f(resLoc, canvas.width, canvas.height);
 
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        // create empty slot in gpu
+        const texture = gl.createTexture();
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        // set the parameters to render any size image
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        const texLoc = gl.getUniformLocation(program, "u_texture");
+        gl.uniform1i(texLoc, 0); // bind u_texture to slot 0
+
+        image.onload = () => {
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            gl.drawArrays(gl.TRIANGLES, 0, 6);
+        };
     }, [])
 
     return (
